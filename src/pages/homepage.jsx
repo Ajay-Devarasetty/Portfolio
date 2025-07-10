540bd96650fe353aa1c90591ccc4ae9133aec615
ajay <devarasettyajay7@gmail.com>
2025-07-10 17:38:53 +0530

    removing unused helmet import

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Helmet } from "react-helmet";
 
 import { faMailBulk } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
